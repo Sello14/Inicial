7902f6da49665685804ec81ef8b706595fc18e58
Sello14 <joaogabrielrec@hotmail.com>
2024-12-02 16:40:37 +0000

    Create ProgCPP_JV_03.cpp

---
+++ ProgCPP_JV_03.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int idade;
+    float altura;
+	cout << "Introduza sua idade: ";
+	cin >> idade;
+    cout << "Introduza sua altura: ";
+    cin >> altura;
+	cout << "Yor age is: " << idade << " e sua altura: " << altura << endl;
+	return 0;
+}
