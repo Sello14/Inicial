f1e24ec7104a18d0d9aa52a282d2c8208cc8b2d6
Sello14 <joaogabrielrec@hotmail.com>
2024-12-02 16:02:09 +0000

    Create ProgCPP_JV_01.cpp

---
+++ ProgCPP_JV_01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Introduza um numero: ";
+	cin >> number;
+	cout << "O numero introduzido foi: " << number << endl;
+	return 0;
+}
